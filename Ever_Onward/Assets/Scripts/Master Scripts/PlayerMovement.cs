a1044317bbb16de404498205d5e200b4ce6a766e
Danin <tenerid@ferris.edu>
2021-12-08 01:02:19 -0500

    Danin's Assets that were removed
    big one

--- Ever_Onward/Assets/Scripts/Master Scripts/PlayerMovement.cs
+++ Ever_Onward/Assets/Scripts/Master Scripts/PlayerMovement.cs
@@ -11,8 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public Animator springAnimator;
     public float mouseSensitivity = 3.5f;
     public float walkSpeed = 24.0f;
-    public float gravity = -13.0f;
-    [SerializeField]  [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    public float gravity = -20.0f;
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
     [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
 
     public bool lockCursor = true;
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     CharacterController controller = null;
 
     private float timeLeftGrounded = 0;
-    public float jumpHeight = 8;
+    public float jumpHeight = 12;
 
     public Vector3 respawnPosition1;
     GameObject respawnPoint;
@@ -37,16 +37,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 currentMouseDelta = Vector2.zero;
     Vector2 currentMouseDeltaVelocity = Vector2.zero;
 
-    private float delta; 
+    private float delta;
     CameraHandler cameraHandler;
 
-    public bool isGrounded
+    /*public bool isGrounded
     {
         get
         {
             return controller.isGrounded || timeLeftGrounded > 0;
         }
-    }
+    }*/
 
 
 
@@ -55,8 +55,6 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         cameraHandler = CameraHandler.singleton;
-
-        //crounchScale = new Vector3(.5f,.5f, .5f);
 
         //locks cursor to the middle of the screen and remove the icon
         if (lockCursor)
@@ -72,14 +70,14 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        delta  = Time.fixedDeltaTime;
+        delta = Time.fixedDeltaTime;
         if (cameraHandler != null)
         {
             cameraHandler.FollowTarget(delta);
             //cameraHandler.HandleCameraRotation(delta);
         }
         UpdateMouseLook();
-        
+
 
         if (transform.position.y <= -50)
         {
@@ -105,15 +103,15 @@ public class PlayerMovement : MonoBehaviour
         Vector2 targetmouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetmouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
-        
+
         cameraPitch -= currentMouseDelta.y * mouseSensitivity;
         cameraPitch = Mathf.Clamp(cameraPitch, -45.0f, 45.0f);
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-        
+
         transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
 
     }
-   
+
 
     //Character Movement
     void UpdateMovement()
@@ -125,28 +123,26 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
         {
             springAnimator.SetBool("isMoving", false);
+            walkSpeed = 0;
         }
         else
         {
             springAnimator.SetBool("isMoving", true);
+            if (Input.GetKeyDown(KeyCode.LeftShift) && controller.isGrounded)
+            {
+
+                walkSpeed = 30.0f;
+
+            }
+            if (!Input.GetKey(KeyCode.LeftShift) && controller.isGrounded)
+            {
+
+                walkSpeed = 24.0f;
+
+            }
         }
-        bool isJumpHeld = Input.GetButton("Jump");
-        bool onJumpPress = Input.GetButtonDown("Jump");
-        
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && controller.isGrounded)
-        {
-
-            walkSpeed = 30.0f;
-
-        }
-        if (!Input.GetKey(KeyCode.LeftShift) && controller.isGrounded)
-        {
-
-            walkSpeed = 24.0f;
-            
-        }
-
+        //bool isJumpHeld = Input.GetButton("Jump");
+        //bool onJumpPress = Input.GetButtonDown("Jump");
 
         Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         targetDir.Normalize();
@@ -158,15 +154,15 @@ public class PlayerMovement : MonoBehaviour
         {
             velocityY = 0.0f;
             timeLeftGrounded = .2f;
-
-        }
-
-        if (isGrounded)
-        {
-            if (isJumpHeld)
+            if (Input.GetButton("Jump"))
             {
                 velocityY = jumpHeight;
-                timeLeftGrounded = 0;
+                //timeLeftGrounded = 0;
+                springAnimator.SetBool("isJumping", true);
+            }
+            else
+            {
+                springAnimator.SetBool("isJumping", false);
             }
         }
         velocityY += gravity * Time.deltaTime;
@@ -178,5 +174,5 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-  
+
 }
