c5f01dd9c0f747d54d13a2d97e2bacb301763751
Danin <tenerid@ferris.edu>
2021-12-09 01:32:36 -0500

    Pause Update 4
    pause

--- Ever_Onward/Assets/Scripts/Master Scripts/MenuContoller.cs
+++ Ever_Onward/Assets/Scripts/Master Scripts/MenuContoller.cs
@@ -11,6 +11,7 @@ public class MenuContoller : MonoBehaviour
     GameObject quitButton;
     GameObject loading;
     GameObject music;
+    GameObject pauseMenu;
     public void Start()
     {
         optionsMenu = GameObject.Find("Settings");
@@ -19,8 +20,28 @@ public class MenuContoller : MonoBehaviour
         quitButton = GameObject.Find("Quit");
         loading = GameObject.Find("Loading");
         music = GameObject.Find("Music");
-        optionsMenu.SetActive(!optionsMenu.activeSelf);
-        loading.SetActive(!loading.activeSelf);
+        pauseMenu = GameObject.Find("PauseMenu");
+        optionsMenu.SetActive(false);
+        loading.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                optionsMenu.SetActive(false);
+                if (Time.timeScale == 1)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Resume();
+                }
+            }
+        }
     }
     public void Play()
     {
@@ -36,6 +57,11 @@ public class MenuContoller : MonoBehaviour
         optionsButton.SetActive(!optionsButton.activeSelf);
         quitButton.SetActive(!quitButton.activeSelf);
     }
+
+    public void InGameOptions()
+    {
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+    }
     public void Inventory()
     {
 
@@ -44,5 +70,29 @@ public class MenuContoller : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+    public void  Resume()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+    public void ToMenu()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        loading.SetActive(true);
+        music.SetActive(false);
+        SceneManager.LoadSceneAsync("MainMenu");
     }
 }
